1d4c280167961334f4f1c7ec132f7964c6732196
truongdinhtrung <truongdinhtrungob@gmail.com>
2024-08-21 00:29:04 +0700

    test

---
+++ play.js
@@ -0,0 +1,26 @@
+let name ='Max';
+const age = 20;
+let hobbies = true;
+
+name = 'Trung';
+// function summeria(username, userage, userhobbies) {
+//     return (
+//         "Name is:" + username + ', age is: ' + userage + ', my hobbies+ ' + userhobbies
+//     );
+// }
+// console.log(summeria(name, age, hobbies));
+
+// const add1 = (username, userage, userhobbies) => {
+//     return(
+//         "Name is:" + username + ', age is: ' + userage + ', my hobbies+ ' + userhobbies
+//     );
+// }
+// console.log(add1(username, userage, userhobbies));
+
+// const add2= (username, userage, userhobbies) => "Name is:" + username + ', age is: ' + userage + ', my hobbies+ ' + userhobbies
+// console.log(add2(username, userage, userhobbies));
+
+//  const add3= () =>  "Name is:" + username + ', age is: ' + userage + ', my hobbies+ ' + userhobbies
+//  console.log(add3())
+
+
